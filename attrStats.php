2c6f16444007e40ba8c99671f8304d800e92b4dc
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2016-04-08 17:14:37 +0200

    cleanup, send the data via GET to fakeurl

--- attrStats.php
+++ attrStats.php
@@ -1,43 +1,22 @@
-<html>
-<head></head>
-<body>
 <?php
-//first option to use the session variables
-//is the issuer/idp somewhere?
-$path_to_attribute_mapping = "/opt/shibboleth-sp-fastcgi/etc/shibboleth/attribute-map.xml";
-$xml = simplexml_load_file($path_to_attribute_mapping) or die("Error: Cannot create object");
-$xml->registerXPathNamespace("ns", "urn:mace:shibboleth:2.0:attribute-map");
+//set this to the collector
+$url='https://ufal-point-dev.ms.mff.cuni.cz/attrStatsCollect';
 
-$configured_names = array();
-foreach($xml->xpath('//ns:Attribute') as $attribute){
-    //echo $attribute['id'] . "=" . $attribute['name'];
+function request($url, $payload) {
+    #$cmd = "curl -X POST -H 'Content-Type: application/json'";
+    #$cmd .= " -d '" . $payload . "' " . "'" . $url . "'";
+    $payload = base64_encode($payload);
+    $cmd = "curl '$url?data=$payload'";
+    $cmd .= " > /dev/null 2>&1 &";
 
-    //best would be to record stats for 'name' instead of 'id'
-    //but there can be one id for multiple names
-    //and we only get the human readable 'id'
-    //eppn=urn:mace:dir:attribute-def:eduPersonPrincipalName
-    //eppn=urn:oid:1.3.6.1.4.1.5923.1.1.1.6
-    $name = (string)$attribute['id'];
-    $configured_names[$name] = 0;
+    exec($cmd, $output, $exit);
+    return $exit == 0;
 }
-$seen_names = array();
-foreach(array_keys($configured_names) as $name){
-    if(getenv($name)){
-        //try the name as is
-        array_push($seen_names, $name);
-    }else if(getenv(strtoupper('http_'.$name))){
-        //try HTTP_NAME
-        array_push($seen_names, $name);
-    }
-}
-var_dump($seen_names);
-echo "<p />";
-//var_dump($_SERVER);
 
-//second option to access the actual assertion
 $seen_names = array();
 $assertion_count = 0;
 $assertion_count_name = "Shib-Assertion-Count";
+//different name in some envs
 $assertion_count_name_upper = str_replace('-','_',strtoupper("http_".$assertion_count_name)); 
 
 if(getenv($assertion_count_name)){
@@ -46,8 +25,8 @@ if(getenv($assertion_count_name)){
     $assertion_count = (int)getenv($assertion_count_name_upper);
 }
 $assertion_link_attr_name = "Shib-Assertion-"; 
+//different name in some envs
 $assertion_link_attr_name_upper = str_replace('-','_',strtoupper("http_". "Shib-Assertion-")); 
-//echo "<li>assertion_count " . $assertion_count; 
 for($i=$assertion_count; $i > 0; $i--){
     //why would there be more than one assertion?
     $n = 0;
@@ -56,28 +35,23 @@ for($i=$assertion_count; $i > 0; $i--){
     } else{
         $n = $i;
     }
+    //$n is usually 00, or 01
     $assertion_link = "";
     if(getenv($assertion_link_attr_name . $n)){
         $assertion_link = getenv($assertion_link_attr_name . $n);
     }else if(getenv($assertion_link_attr_name_upper . $n)){
         $assertion_link = getenv($assertion_link_attr_name_upper . $n);
     }
-    //echo "<li>assertion_link " . $assertion_link; 
     if(!empty($assertion_link)){
         $assertion_link = str_replace("https://" . getenv("SERVER_NAME") ,"https://localhost",$assertion_link);
+        //TODO add a timeout
         $xml = simplexml_load_file($assertion_link);
         $idp = (string)$xml->xpath('//*[local-name()="Issuer"]')[0];
-        echo "<h2>" . $idp . "</h2>";
         foreach($xml->xpath('//*[local-name()="Attribute"]/@Name') as $name){
             array_push($seen_names, (string)$name);
         }
+        request($url, json_encode($seen_names));
     }
 }
-var_dump($seen_names);
+header('Location: ' . $_GET['return'], true, 302);
 ?>
-<p><a id="return_link" href="<?php echo $_GET['return'] ?>">return</a>
-<script type="text/javascript">
-    window.setTimeout(document.getElementById("return_link").click(), 3000);
-</script>
-</body>
-</html>
