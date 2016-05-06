e37512b0c50e8642a39ef3484bf58679cc76efd2
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2016-05-06 16:37:38 +0200

    send it to the backend

--- aaggr.js
+++ aaggr.js
@@ -1,13 +1,20 @@
-var shibbolethSessionUrl = "https://ufal-point-dev.ms.mff.cuni.cz/Shibboleth.sso/Session",
-    spEntityID = "https://ufal-point-dev.ms.mff.cuni.cz/shibboleth/eduid/sp";
+//FILL THESE
+var shibbolethSessionUrl = "/Shibboleth.sso/Session",
+    spEntityID = "unknown",
+    aggregator_url='https://clarin-aa.ms.mff.cuni.cz/aaggreg/v1/got';
+
 jQuery.get(shibbolethSessionUrl).
     done(function(data){
        var session = jQuery(data);
        var idp = session.find("strong:contains('Identity Provider:')")[0].nextSibling.nodeValue.trim();
-       var attrs = session.find("u:contains('Attributes')").nextAll().map(function(index, el){ return el.innerHTML}).get();
+       var attrs = session.find("u:contains('Attributes')").nextAll().map(function(index, el){ return encodeURIComponent(el.innerHTML)}).get();
        if(idp){
-            var obj = {spEntityID: spEntityID, idpEntityID:idp, attrs:attrs};
-            window.alert(JSON.stringify(obj));
+            var ts = Date("c");
+            attributes_encoded = "attributes[]=" + attrs.join("&attributes[]=");
+            var logUrl = aggregator_url + '?idp=' + idp + '&sp=' + spEntityID + '&timestamp=' + ts + '&' + attributes_encoded + '&source=js_aaggr'; 
+            jQuery.get(logUrl).done(function(){
+                console.log("Succcessfully sent " + logUrl);
+            });
        }
 
 });
