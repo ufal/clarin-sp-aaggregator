1732760a9ada79ee842cf5a6990007e9c5d35731
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-01-09 15:41:01 +0100

    check whether there was a reasonable response

--- aaggr.js
+++ aaggr.js
@@ -1,3 +1,9 @@
+function failedGet(url){
+    return function(err){
+        console.error("Failed to get " + url +"\n" + "Got " + err.status + " " + err.statusText );
+    }
+}
+
 //FILL THESE
 var shibbolethSessionUrl = "/Shibboleth.sso/Session",
     spEntityID = "unknown",
@@ -6,15 +12,20 @@ var shibbolethSessionUrl = "/Shibboleth.sso/Session",
 jQuery.get(shibbolethSessionUrl).
     done(function(data){
        var session = jQuery(data);
-       var idp = session.find("strong:contains('Identity Provider:')")[0].nextSibling.nodeValue.trim();
-       var attrs = session.find("u:contains('Attributes')").nextAll().map(function(index, el){ return encodeURIComponent(el.innerHTML)}).get();
-       if(idp){
-            var ts = new Date().toISOString();
-            attributes_encoded = "attributes[]=" + attrs.join("&attributes[]=");
-            var logUrl = aggregator_url + '?idp=' + idp + '&sp=' + spEntityID + '&timestamp=' + ts + '&' + attributes_encoded + '&source=js_aaggr'; 
-            jQuery.get(logUrl).done(function(){
-                console.log("Succcessfully sent " + logUrl);
-            });
+       var lookFor = "strong:contains('Identity Provider:')";
+       if(session.find(lookFor).length > 0){
+            var idp = session.find(lookFor)[0].nextSibling.nodeValue.trim();
+            var attrs = session.find("u:contains('Attributes')").nextAll().map(function(index, el){ return encodeURIComponent(el.innerHTML)}).get();
+            if(idp){
+                    var ts = new Date().toISOString();
+                    attributes_encoded = "attributes[]=" + attrs.join("&attributes[]=");
+                    var logUrl = aggregator_url + '?idp=' + idp + '&sp=' + spEntityID + '&timestamp=' + ts + '&' + attributes_encoded + '&source=js_aaggr'; 
+                    jQuery.get(logUrl).done(function(){
+                        console.log("Succcessfully sent " + logUrl);
+                    }).fail(failedGet(logUrl));
+            }
+       }else{
+           console.error("Did not find session details in the response of " + shibbolethSessionUrl);
        }
 
-});
+}).fail(failedGet(shibbolethSessionUrl));
